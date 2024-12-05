72fca505ac89dfc8d6297f0dc200cf615111d47b
Karsten Jeppesen <kaje@ucn.dk>
2024-12-05 14:46:33 +0100

    WebAppMVC: Working, ver 3.1

--- WebAppMVC/WebAppMVC/Controllers/HomeController.cs
+++ WebAppMVC/WebAppMVC/Controllers/HomeController.cs
@@ -1,20 +1,65 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using System.Text;
 using WebAppMVC.Models;
+using WebAppMVC.Services;
 
 namespace WebAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        public WeatherDataList weatherData { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;
+        private readonly WeatherService _myService;
+
+
+        public HomeController(ILogger<HomeController> logger, WeatherService myService)
         {
             _logger = logger;
+            _myService = myService;
         }
 
-        public IActionResult Index()
+        public static string DecodeBase64String(string base64EncodedData)
         {
+            // Decode the base64 encoded string to a byte array
+            byte[] base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+
+            // Convert the byte array to a plain text string
+            string decodedString = Encoding.UTF8.GetString(base64EncodedBytes);
+
+            return decodedString;
+        }
+
+        public IActionResult Index(string theaction="none")
+        {
+            // If an access_token was provided, then this part shows how to get at it
+            string? accessToken = HttpContext.GetTokenAsync("access_token").Result;
+            if (accessToken != null)
+            {
+                JObject accessTokenBody = JObject.Parse(DecodeBase64String(accessToken.Split(".", 3)[1]));
+                string name = (string)accessTokenBody["name"];
+                ViewBag.name = name;
+            }
+            try
+            {
+                var data = _myService.GetProtectedDataAsync(accessToken).Result;
+                List<WeatherData> weatherDataList = JsonConvert.DeserializeObject<List<WeatherData>>(data);
+                weatherData = new();
+                weatherData.data = new();
+                foreach (var item in weatherDataList)
+                {
+                    weatherData.data.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            ViewBag.weatherData = weatherData;
             return View();
         }
 
@@ -28,5 +73,19 @@ namespace WebAppMVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    }
+
+    public class WeatherData
+    {
+        public string date { get; set; }
+        public string temperatureC { get; set; }
+        public string temperatureF { get; set; }
+        public string summary { get; set; }
+
+    }
+
+    public class WeatherDataList
+    {
+        public List<WeatherData> data { get; set; }
     }
 }
---
+++ WebAppMVC/WebAppMVC/Handlers/AuthorizationHandler.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Just because the user is authenticated in the frontend doesn’t mean the backend 
+ * will automatically trust the frontend to make requests on behalf of the user.
+ * We need to make sure we attach the access token as a header on every request 
+ * we make to the backend API endpoints that require authorization.
+ * We could do this manually in every request, 
+ * but it’s easier via a delegating handler like this:
+*/
+
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+
+namespace WebAppMVC.Handlers
+{
+    /// <summary>
+    /// Automatically adding the access token from an incomming request to an outgoing request
+    /// </summary>
+    /// <param name="httpContextAccessor"></param>
+    public class AuthorizationHandler(IHttpContextAccessor httpContextAccessor) : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(
+           HttpRequestMessage request,
+           CancellationToken cancellationToken)
+        {
+            var httpContext = httpContextAccessor.HttpContext ??
+                throw new InvalidOperationException("No HttpContext available from the IHttpContextAccessor!");
+
+            var accessToken = await httpContext.GetTokenAsync("access_token");
+
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+     }
+}
